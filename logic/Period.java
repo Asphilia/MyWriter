45e9e46aa2970837b8fc2d9fc3468ad2af62c059
Asphilia <janike@katter-ebe.de>
2019-03-15 20:39:19 +0100

    first structures

---
+++ logic/Period.java
@@ -0,0 +1,11 @@
+package logic;
+
+/**
+ * The Period organizes all logic while the working period.
+ *
+ * @author Asphilia
+ * @version 0.1
+ */
+public class Period{
+
+}
---
+++ view/mainwin.java
@@ -0,0 +1,11 @@
+package view;
+
+/**
+ * The MainWin organizes all Subframes and Tabs of MyWriter
+ *
+ * @author Asphilia
+ * @version 0.1
+ */
+public class MainWin{
+
+}
